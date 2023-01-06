0f3c5d1f07cdb6f1b7d949299b17300722f8d43e
Joaquin Keller <joaquin.keller@gmail.com>
2023-01-06 11:58:39 +0100

    first

---
+++ simple.py
@@ -0,0 +1,22 @@
+import numpy as np
+from time import perf_counter
+
+
+a = np.random.rand(4096,4096)
+b = np.random.rand(4096,4096)
+A=a+b*1j
+a = np.random.rand(4096,4096)
+b = np.random.rand(4096,4096)
+B=a+b*1j
+
+# print(10*'-')
+# A.dot(B)
+print(10*'-')
+
+start_time = perf_counter()
+
+np.tensordot(A,B,(1,0))
+
+end_time = perf_counter()
+
+print(f'perf_counter: {end_time- start_time: 0.2f}s')
