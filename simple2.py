901d579e201de67c23f0eac20524f215870cac1f
Joaquin Keller <joaquin.keller@orange.com>
2023-01-12 13:40:50 +0000

    tests ok

--- simple2.py
+++ simple2.py
@@ -1,14 +1,16 @@
 import numpy as np
 from time import perf_counter
 
-nbqubit=25
+nbqubit=31
+loops=6
+print(f"nbqubit:{nbqubit}, loops:{loops}")
 CZ_matrix = np.diag([1,1,1,-1])
 CZ_tensor = np.reshape(CZ_matrix, (2, 2, 2, 2))
 qstate = np.zeros((2,)*nbqubit, dtype=np.csingle)
 qstate[(0,)*nbqubit] = 1
 
 before = perf_counter()
-for _ in range(10):
+for _ in range(loops):
     q0,q1=np.random.randint(0,nbqubit),np.random.randint(0,nbqubit)
     while q0==q1:
         q1=np.random.randint(0,nbqubit)
