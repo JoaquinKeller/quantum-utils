4396d3ceb3043cad721cab25c92967a92dc8a5c4
Joaquin Keller <joaquin.keller@gmail.com>
2023-03-27 14:38:14 +0200

    set all methods as abstract

--- abstractQregister.py
+++ abstractQregister.py
@@ -3,7 +3,11 @@ from abc import ABC, abstractmethod
 class AbstractQuantumRegister(ABC):
     @abstractmethod
     def __init__(self,nbqubit:int, batchsize:int) -> None: pass
+    @abstractmethod
     def measureAll(self): pass
+    @abstractmethod
     def rz(self,q:int,theta): pass
+    @abstractmethod
     def sx(self,q:int): pass
+    @abstractmethod
     def cz(self,q0:int,q1:int): pass
