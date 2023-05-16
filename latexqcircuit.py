857701894270e56f5fd28c51cbcebc22c36f4381
Joaquin Keller <joaquin.keller@gmail.com>
2023-05-16 08:54:05 +0200

    Generate latex from circuit description

--- latexqcircuit.py
+++ latexqcircuit.py
@@ -1,6 +1,10 @@
 from qcircuit import Qcircuit
 from string import Template
 
+# \usepgfplotslibrary{external} 
+# \tikzexternalize
+
+
 LATEX_DOCUMENT_TEMPLATE = r"""\documentclass[preview]{standalone}
 \usepackage[letterpaper,margin=2cm,landscape]{geometry}
 \usepackage{graphicx}
@@ -8,18 +12,11 @@ LATEX_DOCUMENT_TEMPLATE = r"""\documentclass[preview]{standalone}
 \usepackage{amssymb}
 \usepackage{MnSymbol}
 \usepackage{adjustbox}
-
 \usepackage[braket]{qcircuit}
 %\renewcommand{\familydefault}{\sfdefault}
 
-\newcommand{\losange}{\hspace{0.8em}\lozenge&&}
 \newcommand{\ketzero}{\ket{0}&&}
-\newcommand{\ketone}{\ket{1}&&}
-\newcommand{\ketplus}{\ket{+}&&}
-\newcommand{\ketminus}{\ket{-}&&}
 \newcommand{\qdots}{\dots&&}
-\newcommand{\xygate}[1]{\measure{\!\scriptscriptstyle{#1}\!}}
-\newcommand{\zzgate}{\gate{\!{\scriptscriptstyle{z}\!}}}
 
 \begin{document}
 \pagenumbering{gobble}
@@ -64,13 +61,13 @@ def xparam2latex(index:int, dim:int=0)->str:
 class LatexQcircuit(Qcircuit):
     def __init__(self, s: str) -> None:
         super().__init__(s)
-        print(self.nbparams)
+        # print(self.nbparams)
         nbqubits = self.highestqubit+1
         self.visualcircuit= [[] for _ in range(nbqubits)]
         self.latexqubits = ["" for _ in range(nbqubits)]
         
     def appendgate(self,gate:list, param_id:int=-1)->None:
-        print(param_id)
+        # print(param_id)
         tok=gate[0]
         q0 = gate[1][0]
         if tok in self.__class__.entangling_tokens:
@@ -79,15 +76,16 @@ class LatexQcircuit(Qcircuit):
             for q in range(len(self.visualcircuit)):
                 if q == q0: self.visualcircuit[q].append([gate[0],gate[1][1:]])
                 elif q == q1: self.visualcircuit[q].append(['ctrl',gate[1][2:]])
+                elif q0 < q <q1: self.visualcircuit[q].append([''])
                 else: self.visualcircuit[q].append([])
         else:
             for q in range(len(self.visualcircuit)):
                 if q==q0: self.visualcircuit[q].append([gate[0],gate[1][1:],param_id])
                 else:  self.visualcircuit[q].append([])
     
-    def compact(self):
+    def compact(self, n=9999):
         haschanged=True
-        while haschanged:
+        while haschanged and n>0:
             haschanged=False
             for q in range(len(self.visualcircuit)):
                 line=self.visualcircuit[q]
@@ -102,15 +100,24 @@ class LatexQcircuit(Qcircuit):
                     elif nextgate[0]=='cz':
                         # print('cz')
                         otherqubit = nextgate[1][0]
-                        if not self.visualcircuit[otherqubit][i]:
+                        oktoshift= not self.visualcircuit[otherqubit][i]
+                        # for k in range(q+1,otherqubit):
+                        #     oktoshift = oktoshift and  not self.visualcircuit[k][i]
+                        if oktoshift:
+                            for k in range(q,otherqubit+1):
+                                l = self.visualcircuit[k]
+                                l[i],l[i+1]=l[i+1],l[i]                                
                             otherline = self.visualcircuit[otherqubit]
-                            print(otherline)
-                            assert self.visualcircuit[otherqubit][i+1][0]=='ctrl'
-                            line[i],line[i+1]=line[i+1],line[i]
-                            otherline[i],otherline[i+1]=otherline[i+1],otherline[i]
+                            # print(otherline)
+                            # assert self.visualcircuit[otherqubit][i+1][0]=='ctrl'
+                            # line[i],line[i+1]=line[i+1],line[i]
+                            # otherline[i],otherline[i+1]=otherline[i+1],otherline[i]
                             haschanged=True
-                            print(otherline[i]) 
+                            n-=1
+                            if n<=0: break
+                            # print(otherline[i]) 
         # maxdepth = len(self.visualcircuit[0])
+        if n<=0: return
         maxdepth = -1
         for line in self.visualcircuit:
             for i in range(len(self.visualcircuit[0])-1, -1, -1):
@@ -121,12 +128,22 @@ class LatexQcircuit(Qcircuit):
         for q in range(len(self.visualcircuit)):
             line=self.visualcircuit[q]
             self.visualcircuit[q]=line[:maxdepth+1]
-            
+    
+    def __printvisualcircuit(self):
+        for line in self.visualcircuit:
+            for gate in line:
+                if not gate: print("-", end='')
+                elif not gate[0]: print("|", end='')
+                elif gate[0]=='cz': print("*", end='')
+                elif gate[0]=='ctrl': print("!", end='')
+                else: print(gate[0], end='') 
+            print()
+                
     
     def genlatex(self, permutation:list=[], dim:int=0)-> str:
         if not permutation:
             permutation=self.nbparams*[-1]
-        print(permutation)
+        # print(permutation)
         
         permutation_index=0
         for gate in self.parsed:
@@ -136,16 +153,17 @@ class LatexQcircuit(Qcircuit):
                 permutation_index += 1            
             self.appendgate(gate, param_id=param_id)
         self.compact()
+        self.__printvisualcircuit()
         permutation_index=0
         lparams_index=0
         for q in range(len(self.visualcircuit)):
             for gate in self.visualcircuit[q]:
-                if gate == []:
+                if gate == [] or gate[0]=="":
                     self.latexqubits[q]+=r"&\qw"
                 elif gate[0] == 'r':
                     param_id = permutation[permutation_index]
                     param_id = gate[2]
-                    print('param_id:',param_id)
+                    # print('param_id:',param_id)
                     permutation_index += 1
                     if param_id == -1:
                         self.latexqubits[q]+=r"&\gate{"+lparam2latex(lparams_index)+'}'
@@ -176,7 +194,9 @@ def latex2pdf(name='test.tex',texdir='tex/'):
     wd = os.getcwd()
     os.chdir(texdir)
 
-    cmd = ['pdflatex', '--halt-on-error', '--interaction=nonstopmode', name]
+    # cmd = ['pdflatex', '--halt-on-error', '--interaction=nonstopmode','-shell-escape', name]
+    cmd = ['pdflatex', '--halt-on-error', '--interaction=nonstopmode','--enable-write18', name]
+    cmd = ['pdflatex', '--halt-on-error','--interaction=nonstopmode', name]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     p.wait()
     if not not p.returncode: 
@@ -188,12 +208,13 @@ def latex2pdf(name='test.tex',texdir='tex/'):
     os.chdir(wd)
     
 if __name__=="__main__":
-    lq = LatexQcircuit("r(0,t)r(1,t)cz(0,1)r(2,t)cz(1,2)r(0,t)r(1,t)r(2,t)cz(0,2)")
-    lq = LatexQcircuit(Qcircuit.Afalcon120t) 
-    latexdocument = lq.genlatex(permutation=list(range(lq.nbparams)), dim=4)
+    lq = LatexQcircuit("r(0,t)r(1,t)cz(0,1)r(1,t)r(2,t)cz(1,2)r(0,t)r(1,t)r(1,t)r(2,t)cz(0,3)r(0,t)cz(0,2)")
+    # lq = LatexQcircuit(Qcircuit.Bfalcon120t)  
+    # latexdocument = lq.genlatex(permutation=list(range(lq.nbparams)), dim=4)
+    latexdocument = lq.genlatex(dim=1)
     # for qline in lq.visualcircuit: print(qline)
     # for s in lq.latexqubits: print(s)
     with open('tex/test.tex','w') as f: f.write(latexdocument)
-    
+
     # latex2pdf()
         
